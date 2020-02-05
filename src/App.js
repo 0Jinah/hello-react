8b5c083da8ca8bc513f5b4ed28d88b98044c79cd
LeeDongjin <ldj0412@kstec.co.kr>
2020-02-05 10:52:26 +0900

    Counter.js 추가

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Fragment} from 'react';
+//import logo from './logo.svg';
 import './App.css';
+import MyName from "./MyName";
+import MyNameFunc from "./MyNameFunc";
+import Counter from "./Counter";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const name = 'React';
+    const style = {
+        backgroundColor: 'black',
+        padding: '16px',
+        color: 'white',
+        fontSize: '12px'
+    };
+
+    return (
+        <Fragment>
+            <div>
+                <input type="text"></input>
+            </div>
+            <div>
+                Hello {name}
+            </div>
+            {1 + 1 === 2 ? (<div>True!</div>) : (<div>False!</div>)}
+            {1 + 1 === 3 ? (<div>True!</div>) : (<div>False!</div>)}
+
+            <div style={style}>
+                Hi there!
+            </div>
+
+            <div className="App">
+                리액트
+            </div>
+
+            <MyName name={"리액트"}/>
+            <MyName/>
+
+            <MyNameFunc name={"리액트 투"}/>
+            <MyNameFunc/>
+
+            <Counter/>
+        </Fragment>
+    );
 }
 
 export default App;
---
+++ src/MyNameFunc.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyNameFunc = ({name}) => {
+    return (
+        <div>
+            안녕하세요, 제 이름은 {name} 입니다.
+        </div>
+    );
+};
+
+export default MyNameFunc;
